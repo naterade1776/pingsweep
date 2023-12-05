266d70ed5e4aacdfdb7dbfadfde94d9ab2a5f33c
Nate K <97563728+naterade1776@users.noreply.github.com>
2023-12-04 19:34:10 -0700

    Update sweeper.sh
    added ability to fetch hostnames of devices found

--- sweeper.sh
+++ sweeper.sh
@@ -1,5 +1,5 @@
 ifconfig
-
+printf "IP addresses will be displayed in reverse. Why? I don't know."
 printf "Enter first octet of LAN address:\n"
 read o1
 printf "Enter second octet:\n"
@@ -7,4 +7,4 @@ read o2
 printf "Enter third octet:\n"
 read o3
 
-for i in $(seq 254); do ping ${o1}.${o2}.${o3}.${i} -c1 -W1 & done | grep from
+for i in $(seq 254); do nslookup ${o1}.${o2}.${o3}.${i} & done | grep name
